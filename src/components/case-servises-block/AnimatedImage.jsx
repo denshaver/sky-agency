082112257e87f4d22ed3f6caac357f82a312e782
Den Vishchuk <dvishchuk10@gmail.com>
2023-03-15 11:20:06 +0200

    Services animation update

--- src/components/case-servises-block/AnimatedImage.jsx
+++ src/components/case-servises-block/AnimatedImage.jsx
@@ -4,20 +4,16 @@ import { useInView } from "react-intersection-observer";
 
 const AnimatedImage = ({ src, alt, cName, duration }) => {
   const [ref, inView] = useInView({
-    threshold: 0.1,
+    threshold: 0.5,
     triggerOnce: true,
   });
 
   const styles = useSpring({
     to: {
       opacity: inView ? 1 : 0,
-      transform: inView
-        ? "translateX(0px) rotate(-8.28deg)"
-        : "translateX(200px)",
     },
     from: {
       opacity: 0,
-      transform: "translateX(200px) rotate(-8.28deg)",
     },
     config: {
       duration,
